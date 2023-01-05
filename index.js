29cd7a1a5da75d210d04e46ac5c002143319bf86
devKiratu <dev.kiratu@gmail.com>
2023-01-05 12:20:45 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const display = document.querySelector("#display");
+const bar = document.querySelector(".bar");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const value = parseInt(input.value);
+  display.innerHTML = `${value}%`;
+
+  fillBar(value);
+  form.reset();
+}
+
+function fillBar(input) {
+  let color = "green";
+  if (input > 50 && input <= 79) {
+    color = "orange";
+  } else if (input >= 80) {
+    color = "red";
+  }
+  bar.style.width = `${input}%`;
+  bar.style.backgroundColor = color;
+}
